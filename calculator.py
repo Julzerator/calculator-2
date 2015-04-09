92705e2f9a9f362e1095779863e656c51dadf5ee
Hackbright Student <info@hackbrightacademy.com>
2015-04-09 12:31:28 -0700

    Added ascii picture

--- calculator.py
+++ calculator.py
@@ -8,6 +8,24 @@ calculator program yourself in this file.
 from arithmetic import *
 import sys
 
+#Calculator Art:
+#by Jeremy J. Olson http://www.chris.com/ascii/index.php?art=objects/calculators
+print " _____________________ "
+print "|  _________________  |"
+print "| |   HACKBRIGHT X  | |"
+print "| |_________________| |"
+print "|  ___ ___ ___   ___  |"
+print "| | 7 | 8 | 9 | | + | |"
+print "| |___|___|___| |___| |"
+print "| | 4 | 5 | 6 | | - | |"
+print "| |___|___|___| |___| |"
+print "| | 1 | 2 | 3 | | x | |"
+print "| |___|___|___| |___| |"
+print "| | . | 0 | = | | / | |"
+print "| |___|___|___| |___| |"
+print "|_____________________|"
+
+
 
 # Your code goes here
 while True:
